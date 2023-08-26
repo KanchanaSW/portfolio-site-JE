a349cfb46554676095300978b0eb518b4a275a7d
Kanchana Walagambahu <kanchana.walagambahu@axiatadigitallabs.com>
2023-08-26 09:29:55 +0530

    updated

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -9,7 +9,9 @@ const Navbar = ({selectedPage,setSelectedPage}) => {
 
   return (
     <nav className={`z-40 w-full fixed top-0 py-6`}>
-        <div className="flex items-center justify-between mx-auto w-5/6"></div>
+        <div className="flex items-center justify-between mx-auto w-5/6">
+            
+        </div>
     </nav>
   )
 }
