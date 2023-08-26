fdb599953ed2ea61e5815379881c0194a901ee8a
Kanchana Walagambahu <kanchana.walagambahu@axiatadigitallabs.com>
2023-08-26 14:01:32 +0530

    My skills page done

---
+++ src/components/LineGradient.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LineGradient = ({width = "w-full"}) => {
+  return (
+    <div className={`h-5 ${width} bg-gradient-rainblue`}>
+
+    </div>
+  )
+}
+
+export default LineGradient
