359e928a4de958f174d846cfe86740157a3b0984
Kanchana Walagambahu <kanchana.walagambahu@axiatadigitallabs.com>
2023-08-26 10:58:23 +0530

    landing page started

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useMediaQuery from './hooks/useMediaQuery'
 import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState('home')
   const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
+
+  useEffect(()=>{
+    const handelScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true)
+      if (window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener("scroll" , handelScroll)
+    return () => window.removeEventListener("scroll" , handelScroll)
+  },[])
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreen && (
+          <DotGroup selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
     </div>
   );
 }
